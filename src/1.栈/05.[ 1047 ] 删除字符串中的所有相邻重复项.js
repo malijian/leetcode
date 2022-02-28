5343e3c7600464c9a44c9bd6cc259f4954752f28
马黎健 <mlj01178791@alibaba-inc.com>
2022-02-28 11:14:13 +0800

    两道题

--- src/1.栈/05.[ 1047 ] 删除字符串中的所有相邻重复项.js
+++ src/1.栈/05.[ 1047 ] 删除字符串中的所有相邻重复项.js
@@ -2,14 +2,29 @@
  * * 题目名称：删除字符串中的所有相邻重复项
  * * 题目地址：https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string
  */
+ 
+// * 思路： 从左向右依次循环，如果遇到一样的 就pop()删除前一项，并且不要这个值（abbaca）-》（aaca），
 
-// * 思路：
 
-
+var removeDuplicates = function(s) {
+    let arr =[]
+       for (const i in s) {
+           if (arr.length===0) {
+            arr.push(s[i])
+           }else{
+               if ( arr[arr.length-1]!==s[i]) {
+                arr.push(s[i])
+               }else {
+                arr.pop()
+               }
+           }
+       }
+       return arr.join('')
+};
 
 // 测试用例
-let test = ''
+let test = 'abbaca'
 
 console.time('执行用时');
-console.log(xxx(test));
+console.log(removeDuplicates(test));
 console.timeEnd('执行用时');
