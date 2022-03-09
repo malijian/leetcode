fe19c7beb31ffc40dc67f579f1ea5e8ccacd1143
马黎健 <mlj01178791@alibaba-inc.com>
2022-03-09 14:46:52 +0800

    字符串练习提交

--- src/3.字符串/02.[ 14 ] 最长公共前缀.js
+++ src/3.字符串/02.[ 14 ] 最长公共前缀.js
@@ -5,11 +5,37 @@
 
 // * 思路：
 
-
+var longestCommonPrefix = function(strs) {
+    let re=''
+    if (strs.length===0) {
+            return ''
+    }
+    if (strs.length===1) {
+        return  strs.join('')
+    }
+    for (let i = 0; i < strs[0].length; i++) {
+       for (let j = 0; j < strs.length; j++) {
+            if (strs[j][i]!==strs[0][i]) {
+                return re
+            }
+            if (j==strs.length-1&&i== strs[0].length-1) {
+                // 当走完所有的情况
+                if (strs[j][i]==strs[0][i]) {
+                        re+=strs[0][i]
+                        return re
+                }
+            }
+       }
+       re+=strs[0][i]
+    }
+    if (!strs[0]) {
+        return ''
+}
+};
 
 // 测试用例
-let test = ''
+let test =  ["",""]
 
 console.time('执行用时');
-console.log(xxx(test));
+console.log(longestCommonPrefix(test));
 console.timeEnd('执行用时');
--- src/3.字符串/03.[ 13 ] 罗马数字转整数.js
+++ src/3.字符串/03.[ 13 ] 罗马数字转整数.js
@@ -4,12 +4,46 @@
  */
 
 // * 思路：
-
-
+/* 
+   
+*/
+var romanToInt = function (s) {
+    let arr = s.split('').map(el => {
+        switch (el) {
+            case 'I':
+                return 1
+            case 'V':
+                return 5
+            case 'X':
+                return 10
+            case 'L':
+                return 50
+            case 'C':
+                return 100
+            case 'D':
+                return 500
+            case 'M':
+                return 1000
+        }
+    })
+    let sum = 0
+    while (arr.length > 0) {
+        if (arr.length === 1 || arr[0] >= arr[1]) {
+            sum += arr[0]
+        } else if (arr.length === 0) {
+            return sum
+        } else {
+            sum += arr[1] - arr[0]
+            arr.shift()
+        }
+        arr.shift()
+    }
+    return sum
+};
 
 // 测试用例
-let test = ''
+let test = 'MCMXCIV'
 
 console.time('执行用时');
-console.log(xxx(test));
+console.log(romanToInt(test));
 console.timeEnd('执行用时');
