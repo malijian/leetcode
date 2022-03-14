8f372c961d0b454cd8efa90210d7d08f4cada526
马黎健 <mlj01178791@alibaba-inc.com>
2022-03-14 11:19:08 +0800

    位运算操作

--- src/3.字符串/07.[ 67 ] 二进制求和.js
+++ src/3.字符串/07.[ 67 ] 二进制求和.js
@@ -5,12 +5,25 @@
 
 // * 思路：（没思路）
 var addBinary = function(a, b) {
-   
+   let i =a.length-1
+   let j =b.length-1
+   let sum= 0
+   const arr =[]
+   while (i>=0||j>=0||sum!=0) {
+       const x =i>= 0? a.charAt(i) -'0' : 0
+       const y=j>= 0? b.charAt(j) -'0' : 0
+        const total =x+y+sum
+        arr.push(total%2)
+        sum =Math.floor(total/2)
+       i--
+       j--
+   }
+   return arr.reverse().join('')
 };
 
 
 // 测试用例
-let test = '1111'
+let test = '11'
 let  b= '1'
 
 console.time('执行用时');
--- src/3.字符串/13.[ 415 ] 字符串相加.js
+++ src/3.字符串/13.[ 415 ] 字符串相加.js
@@ -5,23 +5,34 @@
 
 // * 思路：
 
-var addStrings = function(num1, num2) {
-    let sum =''
-    for (let i = 0; i < num2.length; i++) {
-       if (+num2[i]+(+num1[i])===10) {
-        sum+='0'
-       }else {
-          
-        sum+= (Number(num2[i])+Number(num1[i])).toString()
-       }
-        
+
+/* 
+        1  初始两数字的length，一个存是否进一的sum  一个返回接口的数组
+        2.  通过while判断 ，通过charAt根据索引找字符，位数较短的补0，
+        3.  进行末尾相加 ，如果需要进一  则记录，将和push到返回数组中   
+        4.   将数组进行翻转并转换成字符串即可
+*/
+
+var addStrings = function (num1, num2) {
+    let i = num1.length-1
+    let j = num2.length-1
+    let sum = 0
+    let arr =[]
+    while (i >=0 || j >= 0 || sum !== 0) {
+        let x = i >= 0 ? num1.charAt(i) - '0' : 0
+        let y = j >= 0 ? num2.charAt(j) - '0' : 0
+         let result =x+y+sum
+        sum =Math.floor( result/10)
+        arr.push(result%10)
+        i-=1
+        j-=1
     }
-    console.log(sum,'xx');
+    return arr.reverse().join('')
 };
 
 // 测试用例
-let test = '117'
-let test2 = '123'
+let test = '7'
+let test2 = '129'
 console.time('执行用时');
-console.log(addStrings(test,test2));
+console.log(addStrings(test, test2));
 console.timeEnd('执行用时');
